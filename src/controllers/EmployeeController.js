d7b6f5ecc624b96076b9f20d15164dea506db5c0
Agustin Ignacio Kanner <agustinkanner@gmail.com>
2016-12-19 16:08:12 -0300

    Complete las operaciones CRUD de los empleados
    [*] Agregue la Operacion DELETE
    [*] Agregue la Operacion PUT (Update)

--- src/controllers/EmployeeController.js
+++ src/controllers/EmployeeController.js
@@ -68,6 +68,12 @@ exports.findById = function(req, res) {
 exports.addEmployee = function(req, res) {  
     //valida el requerimiento
     req.check(empValidationsSchema);
+    //verfica que el id enviado sea un id valido  
+    var validId = security.getValidId(req.params.id);
+    if (validId == false) 
+    {
+        return res.status(412).json({message: "Id inválido"});
+    }
     //getValidationResult devuelve una promesa
     req.getValidationResult().then(function(result) {
         if (!result.isEmpty()) 
@@ -84,10 +90,86 @@ exports.addEmployee = function(req, res) {
     });
 };
 
+//PUT - Actualiza un empleado
+exports.updateEmployee = function(req, res) {  
+
+     //valida el requerimiento
+    req.check(empValidationsSchema);
+     //getValidationResult devuelve una promesa
+    req.getValidationResult().then(function(result) {
+        if (!result.isEmpty()) 
+        {
+          //si hay algun error se le informa al usuario
+          res.status(400).send('There have been validation errors: ' + util.inspect(result.array()));
+        }
+        else
+        {
+            //si no hay errores se crea el nuevo empleado
+            updateEmployee(req,res)
+        }
+    });
+};
+
+
+//DELETE - Elimina un empleado de la base de datos
+exports.deleteEmployee = function(req, res) {  
+     //valida el requerimiento
+    req.check(empValidationsSchema);
+    //verfica que el id enviado sea un id valido  
+    var validId = security.getValidId(req.params.id);
+    if (validId == false) 
+    {
+        return res.status(412).json({message: "Id inválido"});
+    }
+    Employee.findById(req.params.id, function(err, emp) {
+        if(err)
+        {
+             //si hay algun error se le informa al usuario
+            logger.error("no se pudo encontrar al empleado con id " + req.params.id + " : " + err);
+            res.status(500).send(err.message);
+            return;
+        }
+        if(!emp)
+        {
+            //si no existe el empleado
+             res.status(404).send("there is no employee with id: " + req.params.id);
+            return;
+        }
+        //intenta borrar al empleado
+        try
+        {
+          removeEmployee(emp);
+          res.status(200).json(emp);
+        }
+        catch(err)
+        {
+            logger.error(err.message);
+            res.status(500).send(err.message);
+        }
+       
+    });
+};
+
 
 //FIN VERBOS
 
-
+/**
+ * Elimina el empleado de la base de datos
+ *
+ * @param emp   Entidad Employee a eliminar
+ *
+ */
+function removeEmployee(emp)
+{
+    var error;
+     emp.remove(function(err) {
+        error = err; //lanza excepcion
+     });
+    if(error)
+    {
+        throw error;
+    }
+}
 /**
  * Crea el empleado nuevo y devuelve la respuesta al usuario
  * 
@@ -102,6 +184,48 @@ function createEmployee(req,res)
         ename:     req.body.ename,
     });
     //trata de guardar al empleado recien creado
+    saveEmployeeAndInformResult(emp,res);
+}
+/**
+ * Actualiza un empleado en la base de datos
+ *
+ * @param req   Objeto HTTP Request
+ * @param res   Objeto HTTP Response
+ *
+ */
+function updateEmployee(req,res)
+{
+     //busca el empleado en la base de datos
+     var emp = Employee.findById(req.params.id,function(err,emp){
+         if(err)
+         {
+            //si hay algun error se le informa al usuario
+            logger.error("no se pudo encontrar al empleado con id " + req.params.id + " : " + err);
+            res.status(500).send(err.message);
+            return;
+         }
+         if(!emp)
+         {
+            res.status(404).send("there is no employee with id: " + req.params.id);
+            return;
+         }
+         emp.title = security.cleanup(req.body.title);
+         emp.ename = security.cleanup(req.body.ename);
+         //guarda los cambios
+         saveEmployeeAndInformResult(emp,res);
+     });
+    
+
+}
+/**
+ * Guarda el usuario y escribe el resultado en la respuesta HTTP
+ *
+ * @param emp   Entidad Employee a guardar
+ * @param res   Objeto HTTP Response
+ *
+ */
+function saveEmployeeAndInformResult(emp,res)
+{
     emp.save(function(err, emp) {
         if(err) 
         {
@@ -110,7 +234,7 @@ function createEmployee(req,res)
             res.status(500).send( err.message);
             return; 
         }
-    //si sale todo ok se le envia el nuevo empleado al usuario
-    res.status(200).json(emp);
+        //si sale todo ok se le envia el nuevo empleado al usuario
+        res.status(200).json(emp);
     });
 }
