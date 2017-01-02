4555b7a23f7c2b0f22aa18c8974e8a4ab5a943d1
Agustin Ignacio Kanner <agustinkanner@gmail.com>
2017-01-02 10:21:28 -0300

    Agregue la referencia a la entidad payments

--- src/models/employee.js
+++ src/models/employee.js
@@ -1,11 +1,18 @@
 var mongoose = require('mongoose'),  
     Schema   = mongoose.Schema;
 
+var idvalidator = require('mongoose-id-validator');
 //sets schema
 
 var employeeSchema = new Schema({	  
   ename: String,
-  title: String
+  title: {type: mongoose.Schema.Types.ObjectId,
+    ref: 'Payment'}
 });
+
+
+//se asegura que los ids de emp_id y proj_id existan antes de guardar el modelo
+employeeSchema.plugin(idvalidator,{message : 'Bad ID value for title'});
+
 //expors schema
 module.exports = mongoose.model('Employee', employeeSchema); 
